579438a0ba31e8498cc095d6dae07f3ff8947efe
senno adi <senno.adi2509@gmail.com>
2022-10-28 14:24:02 +0700

    init commit

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,10 +3,10 @@ import '../unique_file/style_dictionary_color.dart';
 
 class ColorDictionary {
   ColorDictionary._();
-  static const backgroundDisabled = adrColor.backgroundDisabled;
-  static const backgroundPlaceholder = adrColor.backgroundPlaceholder;
-  static const backgroundWhite = adrColor.backgroundWhite;
-  static const errorBase = adrColor.errorBase;
+  static const backgroundDisabled = adrColor.backgroundDisable;
+  static const backgroundPlaceholder = adrColor.backgroundBase;
+  static const backgroundWhite = adrColor.backgroundLight;
+  static const errorBase = adrColor.backgroundError;
   static const errorLight = Color(0xFFFEE4E2);
   static const grayBase = Color(0xFF98A2B3);
   static const grayDark = Color(0xFF344054);
@@ -15,11 +15,11 @@ class ColorDictionary {
   static const linkBase = Color(0xFF1570EF);
   static const linkLight = Color(0xFFB2DDFF);
   static const outlineGray = Color(0xFF344054);
-  static const primaryBase = adrColor.primaryBase;
+  static const primaryBase = adrColor.backgroundPrimary;
   static const primaryLight = Color(0xFFFEF5CB);
   static const successBase = Color(0xFF12B76A);
   static const successLight = Color(0xFFD1FADF);
-  static const textBlack = adrColor.textBlack;
+  static const textBlack = adrColor.typeNormal;
   static const textGray = Color(0xFF98A2B3);
   static const textWhite = Color(0xFFFCFCFD);
   static const warningBase = Color(0xFFF79009);
